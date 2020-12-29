f806495a4b7aae69ebb10fa3f6c21e232651a906
wanida <wanidarasen098@gmail.com>
2020-12-29 11:43:51 +0700

    Lab 2 : types and import data

---
+++ src/LAB2/BasiclnputData.java
@@ -0,0 +1,25 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class BasiclnputData {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer:");
+        int num = scanner.nextInt();
+
+
+        System.out.println("The number is "+num);
+
+
+
+
+
+
+
+    }//main
+
+}//calss
---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,31 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("Enter message:");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Enter integer :");
+        String std_id = scanner.next();
+
+        System.out.println("Enter integer :");
+        int Age = scanner.nextInt();
+
+        System.out.println("Enter integer :");
+        String tel = scanner.next();
+
+        System.out.println("Enter message :");
+        String email = scanner.next();
+
+
+
+
+
+    }//main
+}//class
